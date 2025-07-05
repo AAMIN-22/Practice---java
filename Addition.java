8b5f024c82a078b0fe85d4f2cce7af2d2517ecbd
AAMIN-22 <aaminshaikh100@gmail.com>
2025-07-05 18:03:49 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,14 @@
+public class Addition {
+    public static void main(String args[]){
+        int as = -5 + 8 * 6;
+        int aam = (55+9) % 9;
+        int sim = 20 + -3*5 / 8;
+        int cls = 5 + 15 / 3 * 2 - 8 % 3;
+        
+        System.out.println("Result => " +(as));
+        System.out.println("Result => " +(aam));
+        System.out.println("Result => " +(sim));
+        System.out.println("Result => " +(cls));
+        
+    }   
+}  
